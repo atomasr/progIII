e98719154ccf27e641c7b624e237f90ef31b05a5
Tomás Rodríguez <augustotomasrodriguez@gmail.com>
2022-04-10 16:29:06 -0300

    Ejercicio 1 finalizado.

---
+++ Practico2/punto_2_1/Main.java
@@ -0,0 +1,49 @@
+package punto_2_1;
+
+public class Main {
+
+	private static int[] arreglo = {1, 2, 3, 4, 5};
+	private static int max = arreglo.length;;
+
+	public static void main(String[] args) {
+		
+		System.out.println("El arreglo " + arreglo + " esta ordenado: " + estaOrdenado(0));
+		// Stack Overflow :(
+	}
+	
+	public static boolean estaOrdenado(int pos) {
+		if ((pos < max)&&(arreglo[pos] <= arreglo[pos+1])) {
+			return estaOrdenado(pos++);
+		} else {
+			if (pos >= max) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+	}
+	public static boolean estaOrdenado2(int pos1, int pos2) {
+		if (pos2 > arreglo.length) {
+			return true;
+		} else if (pos1 < pos2){
+			return estaOrdenado2(pos1++,pos2++);
+		} else {
+			return false;
+		}
+	}
+	
+	/*
+	 * 1) O(n)
+	 * 2) El hilo computacional se puede sobrecargar antes
+	 * 3) public boolean estaOrdenado(Node n, int contador) {
+	 * 		if ((contador < size)&&(n.getValue() < n.getNext().getValue())) {
+	 * 			return estaOrdenado(n.getNext(), contador++); 
+	 * 		} else {
+	 * 			return (contador >= size);
+	 * 		}
+	 * 	}
+	 * 
+	 * */
+	
+	
+}
